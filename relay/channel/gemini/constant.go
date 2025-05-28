e22e94808404c6257b560e7bb12ae81863302235
wans10 <wans10@hotmail.com>
2025-05-28 09:36:45 +0800

    更新 OpenAI , Anthropic , Google , xAI 默认模型列表

--- relay/channel/gemini/constant.go
+++ relay/channel/gemini/constant.go
@@ -2,22 +2,18 @@ package gemini
 
 var ModelList = []string{
 	// stable version
-	"gemini-1.5-pro", "gemini-1.5-flash", "gemini-1.5-flash-8b",
-	"gemini-2.0-flash",
-	// latest version
-	"gemini-1.5-pro-latest", "gemini-1.5-flash-latest",
+	"gemini-2.0-flash-001",
+	"gemini-2.0-flash-lite-001",
+	"gemini-1.5-flash",
+	"gemini-1.5-flash-8b",
+	"gemini-1.5-pro",
 	// preview version
-	"gemini-2.0-flash-lite-preview",
-	// gemini exp
-	"gemini-exp-1206",
-	// flash exp
-	"gemini-2.0-flash-exp",
-	// pro exp
-	"gemini-2.0-pro-exp",
-	// thinking exp
-	"gemini-2.0-flash-thinking-exp",
-	"gemini-2.5-pro-exp-03-25",
-	"gemini-2.5-pro-preview-03-25",
+	"gemini-2.5-flash-preview-05-20",
+	"gemini-2.5-flash-preview-05-20",
+	"gemini-2.5-flash-preview-tts",
+	"gemini-2.5-pro-preview-05-06",
+	"gemini-2.5-pro-preview-tts",
+	"gemini-2.0-flash-preview-image-generation",
 	// imagen models
 	"imagen-3.0-generate-002",
 	// embedding models
--- relay/channel/xai/constants.go
+++ relay/channel/xai/constants.go
@@ -2,17 +2,22 @@ package xai
 
 var ModelList = []string{
 	// grok-3
-	"grok-3-beta", "grok-3-mini-beta",
-	// grok-3 mini
-	"grok-3-fast-beta", "grok-3-mini-fast-beta",
+	"grok-3",
+	"grok-3-fast",
+	"grok-3-mini",
+	"grok-3-mini-fast",
 	// extend grok-3-mini reasoning
-	"grok-3-mini-beta-high", "grok-3-mini-beta-low", "grok-3-mini-beta-medium",
-	"grok-3-mini-fast-beta-high", "grok-3-mini-fast-beta-low", "grok-3-mini-fast-beta-medium",
+	"grok-3-mini-high",
+	"grok-3-mini-low",
+	"grok-3-mini-medium",
+	"grok-3-mini-fast-high",
+	"grok-3-mini-fast-low",
+	"grok-3-mini-fast-medium",
 	// image model
-	"grok-2-image",
+	"grok-2-image-1212",
 	// legacy models
-	"grok-2", "grok-2-vision",
-	"grok-beta", "grok-vision-beta",
+	"grok-2-1212",
+	"grok-2-vision-1212",
 }
 
 var ChannelName = "xai"
